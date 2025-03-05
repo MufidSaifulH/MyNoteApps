80c3e97f8753acf37e65a4755ad350177fe44a7d
mufid <mufidsaifulh@gmail.com>
2025-03-05 21:48:08 +0700

    Membuat SQLite

---
+++ app/src/main/java/com/example/mynoteapps/db/NoteHelper.kt
@@ -0,0 +1,63 @@
+package com.example.mynoteapps.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import com.example.mynoteapps.db.DatabaseContract.NoteColums.Companion._ID
+import kotlin.jvm.Throws
+
+class NoteHelper (context: Context) {
+
+    private var databaseHelper : DatabaseHelper = DatabaseHelper(context)
+    private lateinit var database : SQLiteDatabase
+
+    companion object {
+        private const val DATABASE_TABLE = "dbnoteapp"
+        private var INSTANCE : NoteHelper? = null
+        fun getInstance(context: Context): NoteHelper =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: NoteHelper(context)
+            }
+    }
+    @Throws(SQLiteException::class)
+    fun open() {
+        database = databaseHelper.writableDatabase
+    }
+    fun close() {
+        databaseHelper.close()
+        if (database.isOpen)
+            database.close()
+    }
+    fun queryAll(): Cursor {
+        return database.query(
+            "dbnoteapp",
+            null,
+            null,
+            null,
+            null,
+            null,
+            "$_ID ASC")
+    }
+    fun queryById(id: String): Cursor {
+        return database.query(
+            "dbnoteapp",
+            null,
+            "$_ID = ?",
+            arrayOf(id),
+            null,
+            null,
+            null,
+            null)
+    }
+    fun insert(values : ContentValues?): Long {
+        return database.insert("dbnoteapp",null,values)
+    }
+    fun update(id : String, values: ContentValues?): Int {
+        return database.update("dbnoteapp",values,"$_ID = ?", arrayOf(id))
+    }
+    fun deleteById(id: String): Int {
+        return database.delete("dbnoteapp","$_ID = '$id'",null)
+    }
+}
